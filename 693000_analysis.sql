c03950c1f8069533805d64a30ef27eb2a924abbb
Markus Zabel <markus.zabel@windeln.de>
2017-11-21 23:51:15 +0100

    211117

---
+++ 693000_analysis.sql
@@ -0,0 +1,10 @@
+Select
+	*
+FROM
+	"AX.PROD_DynamicsAX2012.dbo.GENERALJOURNALENTRY" as gj
+LEFT JOIN
+	 "AX.PROD_DynamicsAX2012.dbo.GENERALJOURNALACCOUNTENTRY" as gja
+	 on gj.recid = gja.generaljournalentry 
+Where
+	--gj.subledgervoucher = 'REB0026360'	and
+	left(gja.ledgeraccount,6) = 693000
